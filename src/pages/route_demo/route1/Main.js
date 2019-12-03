dc076355397b7d6c66ddb0cc4903959760d62656
vteso <vtesoho@163.com>
2019-12-03 23:19:28 +0800

    router deom

---
+++ src/pages/route_demo/route1/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Main extends React.Component{
+
+    render(){
+        return (
+            <div>
+                <h1>this is main page </h1>
+            </div>
+        );
+    }
+}
