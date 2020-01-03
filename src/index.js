06dc529f3eed5a786a06ec49e5bf20a6a80309bb
vteso <vtesoho@163.com>
2020-01-03 16:38:47 +0800

    hooks 1

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 // import Life from './pages/demo/Life';
 // import Admin from './admin'
 import Router from './pages/route_demo/route2/router'
+import Hooks from './pages/hooks/hooks'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(<Hooks />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
