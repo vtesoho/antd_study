2b4a775064ff0ffecf7f118f9ee8166ee170d063
vteso <vtesoho@163.com>
2020-01-13 20:12:05 +0800

    react

--- src/pages/hooks/useReducer.js
+++ src/pages/hooks/useReducer.js
@@ -1,17 +1,31 @@
 import React,{useReducer} from 'react'
 
+
+function countReducer(state,action){
+    switch (action.type) {
+        case 'add':
+            return state + 1
+        case 'minus' :
+            return state - 1
+        default:
+            return state
+    }
+}
+
 function MyCountFunc(){
-    const [count,setCount] = useState(0)
+    // const [count,setCount] = useState(0)
+
+    const [count, dispatchCount] = useReducer(countReducer,0)
 
     // setCount(1)   //直接设置一个新的值
     // setCount((c) => c+1)  //这里的c是在setCount执行的时候取到最新的count的值
 
     return (
         <div>
-            <h3>useState</h3>
+            <h3>useReducer</h3>
             <p>{count}</p>
-            <button onClick={()=>{setCount((c) => c+1)}}>在当前值+1</button>
-            <button onClick={()=>{setCount(1)}}>直接设置1</button>
+            <button onClick={()=>{dispatchCount({type:'add'})}}>在当前值+1</button>
+            <button onClick={()=>{dispatchCount({type:'minus'})}}>当前值-1</button>
         </div>
     )
 }
