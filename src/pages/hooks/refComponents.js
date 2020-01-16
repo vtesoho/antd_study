aa33ba3ccbb5fb246911ba7cfdcaf6e3c9911212
vteso <vtesoho@163.com>
2020-01-16 19:55:07 +0800

    ref

---
+++ src/pages/hooks/refComponents.js
@@ -0,0 +1,23 @@
+import React,{useState, useRef, useEffect} from 'react'
+
+function MyCountFunc(){
+    const [count,setCount] = useState(0)
+
+    function testRefFun (){
+        setCount((c) => c+1)
+    }
+
+    useEffect(()=>{
+        
+    })
+
+    return (
+        <div>
+            <h3>RefComponents</h3>
+            <p>Components: {count}</p>
+            <button onClick={()=>{testRefFun()}}></button>
+        </div>
+    )
+}
+
+export default MyCountFunc
--- src/pages/hooks/useRef.js
+++ src/pages/hooks/useRef.js
@@ -1,12 +1,16 @@
 import React,{useState, useRef, useEffect} from 'react'
 
+import RefComponents from './refComponents'
+
 function MyCountFunc(){
     const [count,setCount] = useState(0)
 
     const inputRef = useRef()
 
+    let componentsRef = useRef()
+
     useEffect(()=>{
-        console.log(inputRef)
+        console.log(componentsRef)
     })
 
     return (
@@ -14,6 +18,8 @@ function MyCountFunc(){
             <h3>useRef</h3>
             <p>{count}</p>
             <input ref={inputRef} onChange={()=>{setCount((e)=>{return e+1})}}></input>
+            <hr></hr>
+            <RefComponents ref={componentsRef}></RefComponents>
         </div>
     )
 }
